c7f03afe9d8de15a5bd4ca7ac7a7f83e47ab1f15
James Montemagno <james.montemagno@gmail.com>
2025-05-22 15:29:30 -0700

    Refactor Azure Functions project reference and clean up unused summary mode code in AutoDataSourceForm.razor

--- FeedbackFlow.AppHost/Program.cs
+++ FeedbackFlow.AppHost/Program.cs
@@ -1,9 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var api =builder.AddAzureFunctionsProject<Projects.feedbackfunctions>("feedback-functions");
+var feedbackFunctionsProject = builder.AddAzureFunctionsProject<Projects.feedbackfunctions>("feedback-functions");
 
 builder.AddProject<Projects.feedbackwebapp>("feedback-webapp")
-        .WithEnvironment("FeedbackApi:BaseUrl", api.GetEndpoint("http"))
+        .WithEnvironment("FeedbackApi:BaseUrl", feedbackFunctionsProject.GetEndpoint("http"))
         .WithEnvironment("FeedbackApi:UseMocks", "false");
 
 builder.Build().Run();
